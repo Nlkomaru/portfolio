58cb274c863dd09e27e67fa78c77376820e482bc
nlkomaru <76208219+Nlkomaru@users.noreply.github.com>
2025-01-15 14:19:20 +0900

    :sparkle: Add products carousel

--- src/app/(my-app)/(header)/products/[id]/page.tsx
+++ src/app/(my-app)/(header)/products/[id]/page.tsx
@@ -1,7 +1,15 @@
 import parse from "html-react-parser";
-export default async function Home({ params }: { params: { id: string } }) {
+
+type Params = {
+    params: Promise<{
+        id: string;
+    }>;
+};
+
+export default async function Home({ params }: Params) {
+    const resolvedParams = await params;
     const data: Product = await fetch(
-        `https://nikomaru-portfolio-01.microcms.io/api/v1/products/${params.id}`,
+        `https://nikomaru-portfolio-01.microcms.io/api/v1/products/${resolvedParams.id}`,
         {
             headers: {
                 "X-MICROCMS-API-KEY": process.env.MICROCMS_API_KEY as string,
